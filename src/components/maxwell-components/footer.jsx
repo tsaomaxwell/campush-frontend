c54d5f497c4cc024871e6e214c92022146b567cf
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2020-07-23 21:42:25 -0700

    text in footer change

--- src/components/maxwell-components/footer.jsx
+++ src/components/maxwell-components/footer.jsx
@@ -7,26 +7,25 @@ const Footer = () => {
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
+            {/*<h5 className="title"></h5>*/}
             <p>
-              Here you can use rows and columns here to organize your footer
-              content.
+              built with love by the campush team
             </p>
           </MDBCol>
           <MDBCol md="6">
-            <h5 className="title">Links</h5>
+            {/*<h5 className="title"></h5>*/}
             <ul>
               <li className="list-unstyled">
-                <a href="#!">Link 1</a>
+                <a href="#!">about</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 2</a>
+                <a href="#!">contact</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 3</a>
+                <a href="#!">terms & conditions</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 4</a>
+                <a href="#!">privacy</a>
               </li>
             </ul>
           </MDBCol>
@@ -34,7 +33,7 @@ const Footer = () => {
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> campush </a>
         </MDBContainer>
       </div>
     </MDBFooter>
