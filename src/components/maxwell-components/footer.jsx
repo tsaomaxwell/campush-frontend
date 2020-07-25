3bc8a7ed544251a9e3739ba0042a4e6285d41c6b
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2020-07-24 21:31:01 -0700

    almost done? footer

--- src/components/maxwell-components/footer.jsx
+++ src/components/maxwell-components/footer.jsx
@@ -3,38 +3,27 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Footer = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="6">
-            {/*<h5 className="title"></h5>*/}
-            <p>
-              built with love by the campush team
-            </p>
-          </MDBCol>
-          <MDBCol md="6">
-            {/*<h5 className="title"></h5>*/}
-            <ul>
-              <li className="list-unstyled">
-                <a href="#!">about</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">contact</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">terms & conditions</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">privacy</a>
-              </li>
-            </ul>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> campush </a>
-        </MDBContainer>
+    <MDBFooter color="green" className="font-small pt-4 mt-4">
+      <div id = "footer-box" className="footer-copyright text-center py-3">
+          <p></p>
+          <MDBRow>
+            <MDBCol size = "4"></MDBCol>
+            <MDBCol size ="1">
+                  <a href="#!">about</a>
+            </MDBCol>
+            <MDBCol size ="1">
+                  <a href="#!">contact</a>
+            </MDBCol>
+            <MDBCol size ="1">
+                  <a href="#!">TOS</a>
+            </MDBCol>
+            <MDBCol size ="1">
+                  <a href="#!">privacy</a>
+            </MDBCol>
+            <MDBCol size="4"></MDBCol>
+          </MDBRow>
+          &copy; {new Date().getFullYear()} Copyright: <a href="#!"> campush </a>
+          <p>built with love by the campush team</p>
       </div>
     </MDBFooter>
   );
