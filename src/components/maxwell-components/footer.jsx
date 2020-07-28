3af16baffd3e533c46cfeb99050fd22e18693515
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2020-07-28 16:44:23 -0700

    fixed footer spacing issue

--- src/components/maxwell-components/footer.jsx
+++ src/components/maxwell-components/footer.jsx
@@ -4,11 +4,21 @@ import { MDBCol, MDBRow, MDBFooter } from "mdbreact";
 const Footer = () => {
   return (
     <MDBFooter color="green" className="font-small pt-4 mt-4">
-      <div id = "footer-box" className="footer-copyright text-center py-3">
-          <p></p>
-          <MDBRow>
-            <MDBCol size = "4"></MDBCol>
-            <MDBCol size ="1">
+      <div id="footer-box" className="footer-copyright text-center py-3">
+        <p></p>
+        <MDBRow>
+          <MDBCol size="4"></MDBCol>
+          <MDBCol size="4">
+            <a href="#!">about-</a>
+            <a href="#!">-contact-</a>
+            <a href="#!">-TOS-</a>
+            <a href="#!">-privacy</a>
+          </MDBCol>
+          <MDBCol size="4"></MDBCol>
+        </MDBRow>
+        &copy; {new Date().getFullYear()} Copyright: <a href="#!"> campush </a>
+        <p>built with love by the campush team</p>
+        {/*<MDBCol size ="1">
                   <a href="#!">about</a>
             </MDBCol>
             <MDBCol size ="1">
@@ -20,13 +30,10 @@ const Footer = () => {
             <MDBCol size ="1">
                   <a href="#!">privacy</a>
             </MDBCol>
-            <MDBCol size="4"></MDBCol>
-          </MDBRow>
-          &copy; {new Date().getFullYear()} Copyright: <a href="#!"> campush </a>
-          <p>built with love by the campush team</p>
+            <MDBCol size="4"></MDBCol>*/}
       </div>
     </MDBFooter>
   );
-}
+};
 
 export default Footer;
